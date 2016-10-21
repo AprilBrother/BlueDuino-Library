1ad32f51669efb92b4139e1959df047d59072bed
volca <volca@tom.com>
2016-10-21 15:42:48 +0800

    optimize serial monitor

--- examples/hardwareSerialMonitor/hardwareSerialMonitor.ino
+++ examples/hardwareSerialMonitor/hardwareSerialMonitor.ino
@@ -1,30 +1,19 @@
 #define BAUD_RATE 9600
 
-String tmp; 
-
 void setup() {
-  Serial.begin(BAUD_RATE);
-  Serial1.begin(BAUD_RATE); 
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+    Serial.begin(BAUD_RATE);
+    Serial1.begin(BAUD_RATE); 
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for Leonardo only
+    }
 
-  Serial.println("Hello BlueDuino!");
+    Serial.println("Hello BlueDuino!");
 }
 
 void loop() {
+    if(Serial1.available() > 0)
+        Serial.write(Serial1.read());
 
-  while (Serial1.available() > 0)  {
-    tmp += char(Serial1.read());
-    delay(2);
-  }
-
-  if(tmp.length() > 0) {
-    Serial.println(tmp);
-    tmp = "";
-  }
-
-  if (Serial.available()) {
-    Serial1.write(Serial.read());
-  }
+    if (Serial.available()) 
+        Serial1.write(Serial.read());
 }
