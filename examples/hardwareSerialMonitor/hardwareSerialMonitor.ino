b7a8aac2c8a8f0bfb3be418c8cf760a550dbff9d
yanc <volcas@gmail.com>
2019-02-16 18:02:26 +0800

    add comments

--- examples/hardwareSerialMonitor/hardwareSerialMonitor.ino
+++ examples/hardwareSerialMonitor/hardwareSerialMonitor.ino
@@ -2,6 +2,15 @@
 
 void setup() {
     Serial.begin(BAUD_RATE);
+
+    // Note: The serial function will not work if the device go to sleep.
+    // You need give high level to pin P0.1 to wakeup the device.
+    // https://wiki.aprbrother.com/en/ZeroBeacon.html
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+
+    // BLE module CC2540 Pins - RXI/TXO - Serial1
+    // https://wiki.aprbrother.com/en/BlueDuino_Rev2_Hookup_Guide.html
     Serial1.begin(BAUD_RATE); 
     while (!Serial) {
         ; // wait for serial port to connect. Needed for Leonardo only
